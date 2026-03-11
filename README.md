eae91990c511091ca272a51aa260251ee27f5fb5
KodeUniverse <kodeuniverse@proton.me>
2026-03-11 10:17:40 -0400

    re-named and edit readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# tenor-GIF-for-firefox
-A Firefox browser extension that allows you to access Tenor GIFs to embed GIF images on any social media sites or forums.
+# GIF Fox
+
+## Description
+A Firefox browser extension that allows you to access KLIPY GIFs to embed GIF images on any social media sites or forums.
+
+## Current Status
+Development is underway.
+
+
--- scripts/popup_behavior.js
+++ scripts/popup_behavior.js
@@ -1,42 +1,24 @@
+const API_KEY = process.env.GIF_API_KEY;
 
-
-/**
- * Listen for clicks on the buttons, and send appropriate msg to content script in the page
- * 
- */
-
-const API_KEY = "AIzaSyB6v7LFNrYwcSg5w-VsoLah0-qkWlHhcH4";
-
-
-
-/*function listenForClicks() {
-    document.addEventListener("click", );
-}*/
 //limit is the number of images to fetch
-async function grabGIFs(query, apikey, limit) {
-    print("Fetching from Tenor GIFs");
-    const response = await fetch("https://g.tenor.com/v1/search?q="+query+"&key="+apikey+"&limit="+limit);
+async function fetchGIFs(query, apikey, limit) {
+    print("Fetching GIFs from API...");
+    try {
+        const res = await fetch("https://g.tenor.com/v1/search?q="+query+"&key="+apikey+"&limit="+limit);
 
-    const images_to_display = document.getElementsByClassName("grid-item");
-
-    // code to display them here
-    for (let i = 0; i < response.length; i++){
-        let gif_image = response[i];
-        images_to_display[i].children.src = "../icons/satania-icon-96.png";
+        if (!res.ok)
+            throw new Error(`HTTP ${res.status}: Klipy GIFs API request failed.`)
+        return res.json(); 
+    } catch(error) {
+        console.log(error);
     }
 }
 
 
 browser.runtime.onInstalled.addListener(() => { //getelement returning undefined
     const search_box = document.getElementById("search-box");
-    console.log(typeof(search_box));
     search_box.addEventListener("change", function(event){
-        grabGIFs(search_box.textContent,API_KEY,9);
+        fetchGIFs(search_box.textContent,API_KEY,9);
     });
 });
 
-function reportExecuteScriptError(error) {
-    console.error("Failed to execute content script.");
-}
-
-// Main driver code
