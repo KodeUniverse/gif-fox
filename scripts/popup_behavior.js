11fa3be9220b4252d5466b30331eb5a4120e7543
KodeUniverse <kodeuniverse@proton.me>
2026-03-11 11:54:37 -0400

    verified Klipy API working correctly with fetch and API key.

--- scripts/popup_behavior.js
+++ scripts/popup_behavior.js
@@ -1,13 +1,35 @@
+import "dotenv/config"
+
 const API_KEY = process.env.GIF_API_KEY;
 
+if (API_KEY == null){
+    throw new Error("Could not read API key from .env file.");
+}
+
+const API_GIF_SEARCH = `https://api.klipy.com/api/v1/${API_KEY}/gifs/search`
+
+
 //limit is the number of images to fetch
-async function fetchGIFs(query, apikey, limit) {
-    print("Fetching GIFs from API...");
+async function fetchGIFs(query, options = {
+    per_page: 9,
+    country_code: "us",
+    content_filter: "off",
+    customer_id: 1337 // any value works, there is only one customer since user is expected to have their own free API key.
+}) { 
+
+    console.log("Fetching GIFs from API...");
     try {
-        const res = await fetch("https://g.tenor.com/v1/search?q="+query+"&key="+apikey+"&limit="+limit);
+        
+        const {per_page, locale, country_code, content_filter, customer_id} = options;
+        
+        const reqURL = API_GIF_SEARCH + 
+            `?per_page=${per_page}&q=${query}&customer_id=${customer_id}&locale=${country_code}&content_filter=${content_filter}`
+
+        const res = await fetch(reqURL);
 
         if (!res.ok)
-            throw new Error(`HTTP ${res.status}: Klipy GIFs API request failed.`)
+            throw new Error(`HTTP ${res.status}: Klipy GIFs API request failed. \nRequest URL:${reqURL}`)
+        console.log("GIF Search API request completed!");
         return res.json(); 
     } catch(error) {
         console.log(error);
@@ -15,10 +37,12 @@ async function fetchGIFs(query, apikey, limit) {
 }
 
 
-browser.runtime.onInstalled.addListener(() => { //getelement returning undefined
-    const search_box = document.getElementById("search-box");
-    search_box.addEventListener("change", function(event){
-        fetchGIFs(search_box.textContent,API_KEY,9);
-    });
-});
+console.log(await fetchGIFs("sonic"));
 
+//browser.runtime.onInstalled.addListener(() => { //getelement returning undefined
+//    const search_box = document.getElementById("search-box");
+//    search_box.addEventListener("change", function(event){
+//        fetchGIFs(search_box.textContent,API_KEY,9);
+//    });
+//});
+//
